37ce334db70faa61fdd5c0bf6efc9ade2ea9e18a
oveloz12 <oveloz@icstars.org>
2023-06-21 19:23:15 -0500

    changing broken photo
    changing my image back

---
+++ index.js
@@ -0,0 +1,35 @@
+// VARIABLES
+// let, var, const
+
+//let name = "Olivia";
+
+//name = "Veloz";
+
+//console.log(name); // Print to the console
+
+//console.log("Hello world");
+const name = "pizza";
+const topping = "pineapple";
+/* adding two strings;
+concatenation */
+console.log(name, topping);
+
+// TEMPLATE LITERALS // turns everything to a string
+console.log(`${name} ${topping}`);
+
+console.log("32" + "100" + "string");
+
+// integer
+
+// boolean
+
+// false, undefined, null, 0 will be false statements
+let num = name;
+
+console.log(Boolean(num));
+
+let prompt1 = prompt("What is your name?");
+let whereDidHeGo = prompt("Where did he go?");
+console.log(`${prompt1} went to the ${whereDidHeGo}`);
+
+alert(`${prompt1} went to the ${whereDidHeGo}`);
