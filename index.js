8b5f2fea6b3f268dfa319ae07fc1f5ecd8a60dc3
oveloz12 <oveloz@icstars.org>
2023-06-21 19:40:42 -0500

    Alert and Variables
    added variables and alert

--- index.js
+++ index.js
@@ -8,28 +8,36 @@
 //console.log(name); // Print to the console
 
 //console.log("Hello world");
-const name = "pizza";
-const topping = "pineapple";
+// const name = "pizza";
+// const topping = "pineapple";
 /* adding two strings;
 concatenation */
-console.log(name, topping);
+// console.log(name, topping);
 
 // TEMPLATE LITERALS // turns everything to a string
-console.log(`${name} ${topping}`);
+// console.log(`${name} ${topping}`);
 
-console.log("32" + "100" + "string");
+// console.log("32" + "100" + "string");
 
 // integer
 
 // boolean
 
 // false, undefined, null, 0 will be false statements
-let num = name;
+// let num = name;
 
-console.log(Boolean(num));
+// console.log(Boolean(num));
 
-let prompt1 = prompt("What is your name?");
-let whereDidHeGo = prompt("Where did he go?");
-console.log(`${prompt1} went to the ${whereDidHeGo}`);
+// let prompt1 = prompt("What is your name?");
+// let whereDidHeGo = prompt("Where did he go?");
+// console.log(`${prompt1} went to the ${whereDidHeGo}`);
 
-alert(`${prompt1} went to the ${whereDidHeGo}`);
+// alert(`${prompt1} went to the ${whereDidHeGo}`);
+
+let variableName = prompt("What is your name?");
+let variableAge = prompt("What is your Age?");
+let variableHobby = prompt("What is your Hobby?");
+
+alert(
+  `Hi my name is ${variableName} and my age is ${variableAge} and my faveorite hobby is ${variableHobby}`
+);
