1be520d4d8fda47979a7946e9d6cbf8cc4595f5c
oveloz12 <oveloz@icstars.org>
2023-06-23 15:28:27 -0500

    stressful
    a lot, split comments

--- index.js
+++ index.js
@@ -71,98 +71,194 @@
 // && = and // checks to see if all conditions are true
 // ?? = NULLISH
 
-let x = 0;
-let y = 1;
+// let x = 0;
+// let y = 1;
 
-if (x === 0 && y === 1) {
-  console.log("hello world");
-}
-
-let questComplete = false;
-let questComplete2 = false;
-let slimesDefeated = 0;
-// OR OPERATOR
-
-if (slimesDefeated === 10) {
-  questComplete = true;
-}
-
-let i = 0;
-let k = 1;
-
-if (i === 1 || k === 1) {
-  console.log("hello world");
-}
-
-// if this happens or this
-
-// ELSE; if the statement is false
-
-// let v = 10;
-// let u = 8;
-
-// if (v === 9) {
+// if (x === 0 && y === 1) {
 //   console.log("hello world");
-// } else {
-//   console.log("goodbye world");
 // }
 
-//if (login === true) {
-// redirect to page
-//} else {
-// alert("user doesn't exist");
-//}
+// let questComplete = false;
+// let questComplete2 = false;
+// let slimesDefeated = 0;
+// // OR OPERATOR
 
-// else if // will run if the conditions before it are false and specifies a new condition
-
-// if (login === true) {
-// } else if () {
+// if (slimesDefeated === 10) {
+//   questComplete = true;
 // }
 
-// let userName = prompt("Enter your username");
+// let i = 0;
+// let k = 1;
 
-// if (userName === "olivia123") {
-//   alert("logged in!");
-// } else if (userName === "lucy321") {
-//   alert("logged in!");
-// } else {
-//   alert("no account found");
+// if (i === 1 || k === 1) {
+//   console.log("hello world");
 // }
 
-// let randomNumber = Math.floor(Math.random() * 4);
+// // if this happens or this
+
+// // ELSE; if the statement is false
+
+// // let v = 10;
+// // let u = 8;
+
+// // if (v === 9) {
+// //   console.log("hello world");
+// // } else {
+// //   console.log("goodbye world");
+// // }
+
+// //if (login === true) {
+// // redirect to page
+// //} else {
+// // alert("user doesn't exist");
+// //}
+
+// // else if // will run if the conditions before it are false and specifies a new condition
+
+// // if (login === true) {
+// // } else if () {
+// // }
+
+// // let userName = prompt("Enter your username");
+
+// // if (userName === "olivia123") {
+// //   alert("logged in!");
+// // } else if (userName === "lucy321") {
+// //   alert("logged in!");
+// // } else {
+// //   alert("no account found");
+// // }
+
+// // let randomNumber = Math.floor(Math.random() * 4);
+// // console.log(randomNumber);
+
+// // let arr = ["liv", "kamar", "derrick", "jimmy"];
+
+// // console.log(arr[randomNumber]);
+
+// // //if (arr[randomNumber] === "liv") {
+// //   console.log("Hello world");
+// // } else if (arr[randomNumber] === 'kamar') {
+// // console.log("whats poppin")
+// // }else {
+// //   console.log("tech youre wrong");
+// // }
+
+// // let arr = ["shadi", "lucy", "kite"]
+// // if(arr[0] === "shadi") {
+// //   console.log("shadi");
+// // } else if (arr[1] === "lucy"){
+
+// // }
+// // Challenge: Guess the Number
+// // The program generates a random number between 1 and 10.
+// // The user is prompted to guess the number.
+// // If the user's guess matches the generated number, an alert is shown with a success message.
+// // If the user's guess is incorrect, an alert is shown with a failure message.
+
+// let randomNumber = Math.ceil(Math.random() * 10);
+
 // console.log(randomNumber);
 
-// let arr = ["liv", "kamar", "derrick", "jimmy"];
+// let userQuestion = prompt("Guess a number 1-10");
+// if (userQuestion == randomNumber) {
+//   alert("Congrats Homie");
+// } else {
+//   alert("You're wrong homie");
+// }
+// ///variables and data types
+// //let myName = "Colin"
+// ///console.log(myName)
+// //console.log(typeof myName)
 
-// console.log(arr[randomNumber]);
+// /// conditionals
 
-// //if (arr[randomNumber] === "liv") {
-//   console.log("Hello world");
-// } else if (arr[randomNumber] === 'kamar') {
-// console.log("whats poppin")
-// }else {
-//   console.log("tech youre wrong");
+// /// looping thru an array
+
+// //for(let i = 0; team.lengths; i++){
+// //console.log(i);
+// //}
+
+// ///variables and data types
+
+// let myName = "Lucycan";
+// console.log(myName);
+// console.log(typeof myName);
+// /// conditionals
+// if (myName === "Collin") {
+//   console.log("Hello ${myName}");
+// } else {
+//   console.log(false);
+// }
+// // for
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+// /// arrays and methods
+// //let team = ["Lucycan", "Collin", "Mark", "Cortez"];
+// console.log(team);
+// console.log(team.length);
+// /// push
+// console.log(team.push("Derrick", "John"));
+// console.log(team);
+// /// pop
+// console.log(team.pop());
+// console.log(team);
+// ///join
+// console.log(team.join(""));
+// ///slice returns a new array
+// let team1 = "john,mike,kiersten,lucy";
+// console.log(team1);
+// let team1Slice = team1.slice();
+// console.log(team1Slice);
+// console.log(team.slice(1, 3));
+// ///split
+// let team2 = "john,mike,kiersten,lucy";
+// console.log(team1);
+// let teamSplit = team1.split(",");
+// console.log(teamSplit);
+// /// looping thru an array
+// console.log(team);
+
+// let team = "Lucycan,Collin,Melissa,Cortez,Deisy";
+// let roles = [
+//   "Tech Manager",
+//   "TechFello",
+//   "Placement Manager",
+//   "Program Manager",
+//   "Intern",
+// ];
+
+// let names = ["Lucycan", "Collin", "Melissa", "Cortez", "Deisy"];
+// /// hi my name is Lucycan and I am the Tech Manager
+
+// for (let i = 0; i < team.length; i++) {
+//   console.log(`hello my name is ${names[i]} i am the ${roles[i]} `);
 // }
 
-// let arr = ["shadi", "lucy", "kite"]
-// if(arr[0] === "shadi") {
-//   console.log("shadi");
-// } else if (arr[1] === "lucy"){
+// let roles3 =[];
+// let team3 = [];
+
+// let team2 = prompt("Enter your team names");
+// console.log(team2);
+// let roles2 = prompt("Enter your team roles");
+// console.log(roles2);
+
+// for (let i = 0; i < team.length; i++) {
+// console.log(i);
+// console.log(`Hello `)
 
 // }
-// Challenge: Guess the Number
-// The program generates a random number between 1 and 10.
-// The user is prompted to guess the number.
-// If the user's guess matches the generated number, an alert is shown with a success message.
-// If the user's guess is incorrect, an alert is shown with a failure message.
 
-let randomNumber = Math.ceil(Math.random() * 10);
-
-console.log(randomNumber);
-
-let userQuestion = prompt("Guess a number 1-10");
-if (userQuestion == randomNumber) {
-  alert("Congrats Homie");
-} else {
-  alert("You're wrong homie");
+let rfpTeam = prompt("Enter your team's names:");
+let rfpRoles = prompt("Enter your team's roles:");
+console.log(rfpTeam);
+let rfpTeamSplit = rfpTeam.split(",");
+let rfpRolesSplit = rfpRoles.split(",");
+console.log(rfpTeamSplit);
+console.log(rfpRolesSplit);
+for (i = 0; i < rfpTeamSplit.length; i++) {
+  console.log(
+    `Hello my name is ${rfpTeamSplit[i]} and I am the ${rfpRolesSplit[i]}`
+  );
 }
